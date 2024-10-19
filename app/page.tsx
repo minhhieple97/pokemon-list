a0c4094cfb9dd021b155bd2277c7b7c19811b8b3
hieple <hieplevuc@gmail.com>
2024-10-19 18:48:46 +0700

    feat: Setup suspense boundary for page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-export default async function Home() {
+import { Suspense } from 'react';
+import PokemonList, { PokemonListSkeleton } from '@/components/pokemon/PokemonList';
+import Pagination from '@/components/common/Pagination';
+import { fetchPokemonData } from '@/lib/pokemon';
+import TypeFilter, { TypeFilterSkeleton } from '@/components/pokemon/TypeFilter';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page?: string; type?: string };
+}) {
+  const page = Number(searchParams.page) || 1;
+  const type = searchParams.type || null;
+  const pokemonData = await fetchPokemonData(page, type);
+  const { totalPages } = pokemonData;
   return (
     <div className="container mx-auto px-4 py-8">
       <header className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">Pokemon List</h1>
       </header>
+      <Suspense fallback={<TypeFilterSkeleton />}>
+        <TypeFilter selectedType={type} />
+      </Suspense>
+      <Suspense fallback={<PokemonListSkeleton />} key={`${type}-${page}`}>
+        <PokemonList type={type} page={page} />
+      </Suspense>
+      <Pagination currentPage={page} totalPages={totalPages} />
     </div>
   );
 }
