fc3de0c1eac844a147240548d0b5b1c761a0b233
hieple <hieplevuc@gmail.com>
2024-10-19 18:47:37 +0700

    feat: Setup type filter component

---
+++ components/pokemon/TypeFilter.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from '../ui/skeleton';
+import { TypeList } from './TypeList';
+import { getPokemonTypes } from '@/lib/pokemon';
+
+interface TypeFilterProps {
+  selectedType: string | null;
+}
+
+export default async function TypeFilter({ selectedType }: TypeFilterProps) {
+  const types = await getPokemonTypes();
+  return <TypeList types={types} selectedType={selectedType} />;
+}
+
+export function TypeFilterSkeleton() {
+  return (
+    <div className="mb-4 flex flex-wrap gap-2">
+      {[...Array(10)].map((_, index) => (
+        <Skeleton key={index} className="h-10 w-20" />
+      ))}
+    </div>
+  );
+}
