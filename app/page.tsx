dd1216bbe68a9e827bef4fa3f1730cf670260ec2
hieple <hieplevuc@gmail.com>
2024-10-19 19:17:30 +0700

    feat: Setup alert for no pokemon found

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import PokemonList, { PokemonListSkeleton } from '@/components/pokemon/PokemonLi
 import Pagination from '@/components/common/Pagination';
 import { fetchPokemonData } from '@/lib/pokemon';
 import TypeFilter, { TypeFilterSkeleton } from '@/components/pokemon/TypeFilter';
+import { AlertCircle } from 'lucide-react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
 export default async function Home({
   searchParams,
@@ -12,7 +14,8 @@ export default async function Home({
   const page = Number(searchParams.page) || 1;
   const type = searchParams.type || null;
   const pokemonData = await fetchPokemonData(page, type);
-  const { totalPages } = pokemonData;
+  const { totalPages, pokemon } = pokemonData;
+
   return (
     <div className="container mx-auto px-4 py-8">
       <header className="flex justify-between items-center mb-8">
@@ -21,10 +24,22 @@ export default async function Home({
       <Suspense fallback={<TypeFilterSkeleton />}>
         <TypeFilter selectedType={type} />
       </Suspense>
-      <Suspense fallback={<PokemonListSkeleton />} key={`${type}-${page}`}>
-        <PokemonList type={type} page={page} />
-      </Suspense>
-      <Pagination currentPage={page} totalPages={totalPages} />
+      {pokemon.length === 0 ? (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>No Pokémon Found</AlertTitle>
+          <AlertDescription>
+            No Pokémon match the current filter criteria. Try changing the type or page.
+          </AlertDescription>
+        </Alert>
+      ) : (
+        <>
+          <Suspense fallback={<PokemonListSkeleton />} key={`${type}-${page}`}>
+            <PokemonList type={type} page={page} />
+          </Suspense>
+          <Pagination currentPage={page} totalPages={totalPages} />
+        </>
+      )}
     </div>
   );
 }
