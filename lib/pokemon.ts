268ff1bff39565beb5e85cb80acf715820674682
hieple <hieplevuc@gmail.com>
2024-10-19 15:45:38 +0700

    feat: Setup pokemon api

--- lib/pokemon.ts
+++ lib/pokemon.ts
@@ -9,38 +9,46 @@ export async function fetchPokemonData(page: number, type: string | null) {
   let url: string;
   let totalCount: number;
   let pokemonData: any[];
+  try {
+    if (type) {
+      const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+      const data = await response.json();
+      totalCount = data.pokemon.length;
+      pokemonData = data.pokemon
+        .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+        .map((p: { pokemon: any }) => p.pokemon);
+    } else {
+      url = `https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${
+        (page - 1) * itemsPerPage
+      }`;
+      const response = await fetch(url);
+      const data = await response.json();
+      totalCount = data.count;
+      pokemonData = data.results;
+    }
 
-  if (type) {
-    const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
-    const data = await response.json();
-    totalCount = data.pokemon.length;
-    pokemonData = data.pokemon
-      .slice((page - 1) * itemsPerPage, page * itemsPerPage)
-      .map((p: { pokemon: any }) => p.pokemon);
-  } else {
-    url = `https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${
-      (page - 1) * itemsPerPage
-    }`;
-    const response = await fetch(url);
-    const data = await response.json();
-    totalCount = data.count;
-    pokemonData = data.results;
+    const formattedPokemon = await Promise.all(
+      pokemonData.map(async (p: any) => {
+        const id = parseInt(p.url.split('/').slice(-2, -1)[0]);
+        return {
+          ...p,
+          id,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+        };
+      }),
+    );
+
+    return {
+      pokemon: formattedPokemon,
+      totalCount,
+      totalPages: Math.ceil(totalCount / itemsPerPage),
+    };
+  } catch (error) {
+    console.error('Error fetching Pokemon data:', error);
+    return {
+      pokemon: [],
+      totalCount: 0,
+      totalPages: 0,
+    };
   }
-
-  const formattedPokemon = await Promise.all(
-    pokemonData.map(async (p: any) => {
-      const id = parseInt(p.url.split('/').slice(-2, -1)[0]);
-      return {
-        ...p,
-        id,
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-      };
-    }),
-  );
-
-  return {
-    pokemon: formattedPokemon,
-    totalCount,
-    totalPages: Math.ceil(totalCount / itemsPerPage),
-  };
 }
